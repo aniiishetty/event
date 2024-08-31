20b87301a32094472a3dc8578eccf7a1cf373632
aniiishetty <122535414+aniiishetty@users.noreply.github.com>
2024-08-31 15:36:17 +0530

    Update database.ts

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 import * as dotenv from 'dotenv';
-import College from '../models/College'; // Import the College model
-import Registration from '../models/Registration'; // Import the Registration model
+import {College} from '../models/College'; // Import the College model
+import {Registration} from '../models/Registration'; // Import the Registration model
 
 dotenv.config();
 
