7418f36d58a5db1f085a935d802cb021e50d3e3d
aniiishetty <122535414+aniiishetty@users.noreply.github.com>
2024-09-22 10:08:12 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-
 import RegistrationForm from './components/RegistrationForm';
+import List from './components/List'; // Adjust the path if necessary
 
 function App() {
   return (
     <div className="App">
-      
       <main className="App-main">
-        <RegistrationForm />
+        
+        <List /> {/* Add the List component here */}
       </main>
     </div>
   );
