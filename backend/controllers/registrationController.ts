2bf4a36637e68472ce0e7201b889cb27a23d14e3
aniiishetty <122535414+aniiishetty@users.noreply.github.com>
2024-09-22 21:56:35 +0530

    Update registrationController.ts

--- backend/controllers/registrationController.ts
+++ backend/controllers/registrationController.ts
@@ -475,7 +475,7 @@ export const generateAllRegistrationsPDF = async (req: Request, res: Response) =
                 },
             ],
             order: [['eventId', 'ASC']], // Sort by eventId in ascending order
-            offset: 100, // Skip the first 30 registrations
+            offset: 110, // Skip the first 30 registrations
             limit: 10,  // Limit to the next 30 registrations (31 to 60)
         });
 
